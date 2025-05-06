4c6718c7cab3c79a9cd1cafeed884f481080d9ef
Benjamin McMahon <parachutehappens@gmail.com>
2025-05-06 14:05:40 -0500

    chest animations improved

--- src/app/components/inventory/inventory-window/inventory-window.component.ts
+++ src/app/components/inventory/inventory-window/inventory-window.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-inventory-window',
@@ -7,6 +8,24 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './inventory-window.component.html',
   styleUrls: ['./inventory-window.component.css'],
+  animations: [
+    trigger('grow', [
+      state(
+        'open',
+        style({
+          height: '500px'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px'
+        })
+      ),
+      transition('closed => open', [animate('300ms ease-out')]), // Grow
+      transition('open => closed', [animate('300ms ease-in')]), // Shrink
+    ]),
+  ],
 })
 export class InventoryWindowComponent {
   @Input() isOpen = false;
